0f4cea18f90229fd5e05e086b9c5b32fb6a87b09
Dmitriy Revidchuck <rsyu@yandex.ru>
2016-02-08 14:54:41 +0200

    sql for tables added

--- Uppu4.sql
+++ Uppu4.sql
@@ -3,8 +3,8 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 24, 2015 at 04:19 PM
--- Server version: 5.6.27-0ubuntu1
+-- Generation Time: Feb 08, 2016 at 02:53 PM
+-- Server version: 5.6.28-0ubuntu0.15.10.1
 -- PHP Version: 5.6.11-1ubuntu3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `Uppu4`
 --
-CREATE DATABASE IF NOT EXISTS `Uppu4` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `Uppu4`;
 
 -- --------------------------------------------------------
 
@@ -28,16 +26,15 @@ USE `Uppu4`;
 -- Table structure for table `comments`
 --
 
-DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(11) NOT NULL,
-  `path` varchar(200) DEFAULT NULL,
-  `user` int(11) NOT NULL,
+  `path` varchar(200) DEFAULT NULL COMMENT 'Path to the comment(for tree view)',
+  `user` int(11) NOT NULL COMMENT 'ID of user who left comment',
   `comment` varchar(400) NOT NULL,
   `posted` datetime NOT NULL,
-  `fileId` int(11) NOT NULL,
-  `level` int(11) DEFAULT NULL,
-  `parent` int(11) DEFAULT NULL
+  `fileId` int(11) NOT NULL COMMENT 'ID of the file commented',
+  `level` int(11) DEFAULT NULL COMMENT 'Level of comment(for tree view)',
+  `parent` int(11) DEFAULT NULL COMMENT 'Parent of comment(for tree view)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -46,16 +43,15 @@ CREATE TABLE IF NOT EXISTS `comments` (
 -- Table structure for table `files`
 --
 
-DROP TABLE IF EXISTS `files`;
 CREATE TABLE IF NOT EXISTS `files` (
   `id` int(11) NOT NULL,
-  `name` varchar(200) CHARACTER SET latin1 NOT NULL,
+  `name` varchar(200) NOT NULL,
   `size` int(11) NOT NULL,
   `uploaded` datetime NOT NULL,
-  `comment` varchar(200) CHARACTER SET latin1 DEFAULT NULL,
-  `extension` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
-  `mediainfo` text CHARACTER SET latin1,
-  `uploadedBy` int(11) NOT NULL
+  `comment` varchar(200) DEFAULT NULL,
+  `extension` varchar(20) DEFAULT NULL,
+  `mediainfo` text COMMENT 'Media information of the file(custom type in docrtine)',
+  `uploadedBy` int(11) NOT NULL COMMENT 'ID of the user who uploaded file'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -64,15 +60,15 @@ CREATE TABLE IF NOT EXISTS `files` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
-  `login` varchar(20) CHARACTER SET latin1 NOT NULL,
-  `email` varchar(40) CHARACTER SET latin1 DEFAULT NULL,
+  `login` varchar(20) NOT NULL,
+  `email` varchar(40) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
-  `salt` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
-  `token` varchar(20) CHARACTER SET latin1 NOT NULL,
-  `hash` varchar(100) CHARACTER SET latin1 DEFAULT NULL
+  `salt` varchar(20) DEFAULT NULL,
+  `token` varchar(20) NOT NULL COMMENT 'Token for anonymous file load and anonymous users',
+  `hash` varchar(100) DEFAULT NULL COMMENT 'Password hash',
+  `admin` tinyint(1) DEFAULT NULL COMMENT 'Admin privivliges'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -85,14 +81,17 @@ CREATE TABLE IF NOT EXISTS `users` (
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
-  ADD UNIQUE KEY `path` (`path`);
+  ADD UNIQUE KEY `path` (`path`),
+  ADD KEY `user` (`user`),
+  ADD KEY `fileId` (`fileId`);
 
 --
 -- Indexes for table `files`
 --
 ALTER TABLE `files`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `uploadedBy` (`uploadedBy`);
 
 --
 -- Indexes for table `users`
@@ -122,6 +121,23 @@ ALTER TABLE `files`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`fileId`) REFERENCES `files` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `files`
+--
+ALTER TABLE `files`
+  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`uploadedBy`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
